464427d3a33fe663d411f74cfa919542b72d9cb4
selwynsam <selwynsam13@gmail.com>
2025-03-12 00:17:58 +0530

    feat: add replay page for project-specific custom playground

--- web/src/features/replay/page/index.tsx
+++ web/src/features/replay/page/index.tsx
@@ -1,5 +1,4 @@
-import { FullScreenPage } from "@/src/components/layouts/full-screen-page";
-import Header from "@/src/components/layouts/header";
+import Page from "@/src/components/layouts/page";
 import CustomPlayground from "./custom-playground";
 import { CustomSaveToPromptButton } from "./components/CustomSaveToPromptButton";
 import { CustomResetButton } from "./components/CustomResetButton";
@@ -8,24 +7,25 @@ import { CustomPlaygroundProvider } from "./context";
 export default function ReplayPage() {
   return (
     <CustomPlaygroundProvider>
-      <FullScreenPage>
-        <Header
-          title="Custom Playground"
-          help={{
+      <Page
+        headerProps={{
+          title: "Custom Playground",
+          help: {
             description: "A sandbox to test and iterate your prompts",
             href: "https://langfuse.com/docs/playground",
-          }}
-          actionButtons={
+          },
+          actionButtonsRight: (
             <>
               <CustomSaveToPromptButton />
               <CustomResetButton />
             </>
-          }
-        />
+          ),
+        }}
+      >
         <div className="flex-1 overflow-auto">
           <CustomPlayground />
         </div>
-      </FullScreenPage>
+      </Page>
     </CustomPlaygroundProvider>
   );
 }
